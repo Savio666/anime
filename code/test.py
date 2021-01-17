78607d34e36b39d38a96e5a4938d35dc7e51c7d2
savio666 <savio200172@yahoo.com.hk>
2021-01-17 21:46:29 +0800

    modified test.py

--- code/test.py
+++ code/test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import torch
 from torchvision.utils import make_grid
 
-def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
+def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28), ):
     image_tensor = (image_tensor + 1) / 2
     image_unflat = image_tensor.detach().cpu()
     image_grid = make_grid(image_unflat[:num_images], nrow=5)
